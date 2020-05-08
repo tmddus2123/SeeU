27d2b54b58f78d5cc5de3e626d0921324cf502ce
Song-i <elephant890@naver.com>
2020-05-08 17:55:53 +0900

    songi makes Starting, MyReview Activities. Hide Action Bar.

---
+++ app/src/main/java/com/example/seeu/StartingActivity.java
@@ -0,0 +1,28 @@
+package com.example.seeu;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class StartingActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_starting);
+
+        /* 핸들러로 화면 전환에 딜레이를 걸었음 (300밀리세크) */
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable(){
+            @Override
+            public void run(){
+                /* MainActivity를 새로운 intent로 불러와서 시작함.
+                   현재 intent는 종료 */
+                Intent intent = new Intent(getBaseContext(), MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 300);
+    }
+}
