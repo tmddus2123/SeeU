9fdc118a54c18db759575a21ee7ea915715ae274
Song-i <elephant890@naver.com>
2020-05-09 18:06:21 +0900

    songi changed the design of MainActivity and MyReview. makes three xml files.

--- app/src/main/java/com/example/seeu/MainActivity.java
+++ app/src/main/java/com/example/seeu/MainActivity.java
@@ -8,11 +8,16 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
+import com.example.seeu.ui.login.LoginActivity;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button myRBtn;
-
+    Button myRBtn,loginBtn,logoutBtn,searchBtn;
+    TextView NameTV;
+    Boolean login;
+    String User;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,6 +28,13 @@ public class MainActivity extends AppCompatActivity {
         ab.hide();
 
         myRBtn = (Button)findViewById(R.id.myRBtn);
+        loginBtn = (Button)findViewById(R.id.loginBtn);
+        logoutBtn = (Button)findViewById(R.id.logoutBtn);
+        searchBtn = (Button)findViewById(R.id.searchBtn);
+        NameTV = (TextView)findViewById(R.id.UserName);
+
+        login = true; // 나중에 LoginActivity에서 받아오기!!!!!!
+        User = "See U"; // 나중에 Login하면 User name 받아오기!!!!!
 
         myRBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -30,9 +42,54 @@ public class MainActivity extends AppCompatActivity {
                 /* 내가 쓴 글 버튼을 누르면 내가 쓴 후기 액티비티로 이동 */
                 Intent intent = new Intent(getBaseContext(), MyReview.class);
                 startActivity(intent);
+            }
+        });
+
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                /* 로그인 하기 버튼을 누르면 로그인 액티비티로 이동 */
+                Intent intent = new Intent(getBaseContext(), LoginActivity.class);
+                startActivity(intent);
                 finish();
             }
         });
+
+        logoutBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                /* 로그아웃 버튼을 누르면 로그아웃 후 로그인 액티비티로 이동 */
+
+                /* 로그아웃 하는 코드 추가! */
+                Intent intent = new Intent(getBaseContext(), LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        searchBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                /* 검색하기 버튼을 누르면 좌석 액티비티로 이동 */
+                /*
+                Intent intent = new Intent(getBaseContext(), ConcertActivity.class);
+                startActivity(intent);
+                finish();
+                */
+            }
+        });
+
+        /* 로그인 했으면 로그인 하기 버튼(loginBtn)삭제,
+           비회원이면 로그아웃(logoutBtn), 내가 쓴 글 보기(myRBtn) 삭제 */
+        if(login) {
+            loginBtn.setVisibility(View.GONE);
+            NameTV.setText(User.toString());
+        }
+        else {
+            logoutBtn.setVisibility(View.GONE);
+            myRBtn.setVisibility(View.GONE);
+            NameTV.setText("비회원");
+        }
     }
 
 }
