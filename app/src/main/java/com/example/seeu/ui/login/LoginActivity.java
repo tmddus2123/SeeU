9acfe0c86f1851fbeab810be63193ed52a907626
EunjiShin <simsimhae_98@naver.com>
2020-05-28 15:27:04 +0900

    add userdb & login

--- app/src/main/java/com/example/seeu/ui/login/LoginActivity.java
+++ app/src/main/java/com/example/seeu/ui/login/LoginActivity.java
@@ -11,19 +11,30 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 
 import com.example.seeu.MainActivity;
 import com.example.seeu.R;
 import com.example.seeu.RegisterActivity;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class LoginActivity extends Activity {
 
-
+    private ImageView logoView;
+    private FirebaseAuth mAuth = FirebaseAuth.getInstance();
+    private FirebaseFirestore mStore = FirebaseFirestore.getInstance();
+    private EditText mNicknameView;
+    private EditText mEmailView;
     private EditText mIDView;
     private EditText mPasswordView;
-    private ImageView logoView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,34 +44,13 @@ public class LoginActivity extends Activity {
         // 로그인 폼 생성
 
         mIDView = (EditText) findViewById(R.id.ID);
-        mPasswordView = (EditText) findViewById(R.id.password);
-        logoView = (ImageView)findViewById(R.id.imageView);
+        mNicknameView = (EditText) findViewById(R.id.Nickname);
+        mEmailView = findViewById(R.id.ID);
+        mPasswordView = findViewById(R.id.password);
 
+        logoView = (ImageView) findViewById(R.id.imageView);
         logoView.setImageResource(R.drawable.small_logo);
 
-        // 서버,디비 연동 이후 기능 추가예정. 이벤트 핸들러만 추가
-        mIDView.setOnEditorActionListener(new TextView.OnEditorActionListener()
-        {
-            @Override
-            public boolean onEditorAction(TextView textView, int id, KeyEvent keyEvent) {
-                if(id == EditorInfo.IME_ACTION_DONE || id == EditorInfo.IME_NULL){
-                    return true;
-                }
-                return false;
-            }
-
-        });
-        mPasswordView.setOnEditorActionListener(new TextView.OnEditorActionListener() {
-            @Override
-            public boolean onEditorAction(TextView textView, int id, KeyEvent keyEvent) {
-                if (id == EditorInfo.IME_ACTION_DONE || id == EditorInfo.IME_NULL) {
-                    //attemptLogin();
-
-                    return true;
-                }
-                return false;
-            }
-        });
 
         // 버튼 생성
 
@@ -81,16 +71,29 @@ public class LoginActivity extends Activity {
             }
         });
 
-        mLoginButton.setOnClickListener(new OnClickListener(){
+        mLoginButton.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View view) {
-                /*디비 연동 전 - 로그인 버튼 누르면 메인액티비티로 */
-                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
-                // 로그인 성공했으면 main의 login 값 true로 넘겨주기
-                intent.putExtra("login", true);
-                intent.putExtra("userNickname","SeeU"); //디비에서 로그인한 아이디의 짝인 닉네임불러오기
-                startActivity(intent);
-                finish();
+                mAuth.signInWithEmailAndPassword(mEmailView.getText().toString(), mPasswordView.getText().toString())
+                        .addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+                                    FirebaseUser user = mAuth.getCurrentUser();
+                                    if (user != null) {
+                                        Toast.makeText(LoginActivity.this, "로그인 성공!" , Toast.LENGTH_SHORT).show();
+                                        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                                        intent.putExtra("login", true);
+                                        intent.putExtra("userNickname","SeeU"); //디비에서 로그인한 아이디의 짝인 닉네임불러오기
+                                        startActivity(intent);
+                                        finish();
+                                    }
+                                } else {
+                                    Toast.makeText(LoginActivity.this, "Authentication failed.",
+                                            Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
             }
         });
 
@@ -103,6 +106,7 @@ public class LoginActivity extends Activity {
                 startActivity(intent);
                 finish();
             }
+
         });
 
     }
