f06f73db7f1f48b06824f540da91a1a1bad21867
EunjiShin <simsimhae_98@naver.com>
2020-06-07 22:53:27 +0900

    edit startingActivity

--- app/src/main/java/com/example/seeu/StartingActivity.java
+++ app/src/main/java/com/example/seeu/StartingActivity.java
@@ -2,8 +2,11 @@ package com.example.seeu;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.os.Handler;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.appcompat.app.ActionBar;
 
@@ -16,6 +19,8 @@ public class StartingActivity extends Activity {
     private FirebaseAuth mAuth;
     // 현재 로그인 된 유저 정보
     private FirebaseUser mUser;
+    public ImageView logoView;
+    public ImageView graView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
@@ -24,6 +29,17 @@ public class StartingActivity extends Activity {
         mAuth = FirebaseAuth.getInstance();
         // 현재 로그인한 사용자 정보 받아옴
         mUser = mAuth.getCurrentUser();
+
+        logoView = (ImageView) findViewById(R.id.imageView3);
+        logoView.setImageResource(R.drawable.small_logo);
+
+        TextView text4 = (TextView)findViewById(R.id.textView4);
+        TextView text5 = (TextView)findViewById(R.id.textView5);
+        text4.setTextColor(Color.BLACK);
+        text5.setTextColor(Color.BLACK);
+
+        graView = (ImageView)findViewById(R.id.imageView4);
+        graView.setImageResource(R.drawable.graphic2);
 
         if(mUser != null){
             Handler handler = new Handler();
@@ -36,10 +52,10 @@ public class StartingActivity extends Activity {
                     startActivity(intent);
                     finish();
                 }
-            }, 300);
+            }, 400);
         }
         else {
-            /* 핸들러로 화면 전환에 딜레이를 걸었음 (300밀리세크) */
+            /* 핸들러로 화면 전환에 딜레이를 걸었음 (400밀리세크) */
             Handler handler = new Handler();
             handler.postDelayed(new Runnable() {
                 @Override
@@ -51,7 +67,7 @@ public class StartingActivity extends Activity {
                     startActivity(intent);
                     finish();
                 }
-            }, 300);
+            }, 400);
         }
     }
 }
