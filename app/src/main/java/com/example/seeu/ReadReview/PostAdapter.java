a90e2806c37626b5407d3fc8ecf911bac61e533d
tmddus <tmddus2123@gmail.com>
2020-06-20 05:30:14 +0900

    i can see image!

--- app/src/main/java/com/example/seeu/ReadReview/PostAdapter.java
+++ app/src/main/java/com/example/seeu/ReadReview/PostAdapter.java
@@ -4,11 +4,13 @@ import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.drawable.Drawable;
 import android.net.Uri;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.RatingBar;
@@ -25,6 +27,9 @@ import com.example.seeu.ReadReviewActivity;
 import com.firebase.ui.storage.images.FirebaseImageLoader;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 
@@ -34,6 +39,14 @@ import java.lang.ref.Reference;
 import java.util.ArrayList;
 
 public class PostAdapter extends BaseAdapter {
+
+    FirebaseFirestore db;
+
+    private FirebaseAuth mAuth;
+    private FirebaseUser mUser;
+    FirebaseStorage storage = FirebaseStorage.getInstance();
+    private static final String TAG = "DocSnippets";
+
 
     private ArrayList<Posting> Post=new ArrayList<Posting>();
 
@@ -62,23 +75,46 @@ public class PostAdapter extends BaseAdapter {
            convertView=inflater.inflate(R.layout.custom_listview,parent,false);
        }
 
-        convertView.setOnTouchListener(new View.OnTouchListener() {
-            @Override
-            public boolean onTouch(View v, MotionEvent event) {
-                return true;//리스트뷰 터치 안되게
-            }
-        });
 
-        final ImageView image=(ImageView) convertView.findViewById(R.id.picture);//사진 보여주는거
         TextView review=(TextView)convertView.findViewById(R.id.review);
         TextView nickName=(TextView)convertView.findViewById(R.id.Nickname);
         RatingBar rating=(RatingBar)convertView.findViewById(R.id.ratingBar);
+        Button btn=(Button)convertView.findViewById(R.id.delete);//삭제버튼
 
-        FirebaseStorage firebaseStorage = FirebaseStorage.getInstance();
-        final StorageReference storageReference = firebaseStorage.getReference().child("images/" + Post.get(position).getPicName());
-         Glide.with(context)
-                 .load(storageReference)
-                 .into(image);
+        ImageView image=(ImageView) convertView.findViewById(R.id.picture);//사진 보여주는거
+
+        String id=Post.get(position).getUserID();
+
+        StorageReference storageRef = storage.getReferenceFromUrl("gs://seeu-70a59.appspot.com").child("images/" + Post.get(position).getPicName());
+        Glide.with(context)
+                .load(storageRef)
+                .into(image);
+
+        mAuth = FirebaseAuth.getInstance();
+        mUser = mAuth.getCurrentUser();
+        String uid=mUser.getUid();
+        Log.d(TAG, uid + "같은가요???" + id);
+        if (uid.equals(id)) {//같으면
+            btn.setVisibility(View.VISIBLE);//버튼 보이기?
+        } else {
+            btn.setVisibility(View.GONE);
+            convertView.setOnTouchListener(new View.OnTouchListener() {
+                @Override
+                public boolean onTouch(View v, MotionEvent event) {
+                    return true;//리스트뷰 터치 안되게
+                }
+            });
+        }
+
+        db = FirebaseFirestore.getInstance();
+        /*btn.setOnClickListener(new Button.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG,"삭제");
+            //db.collection("Posting").document()
+                Post.remove(Post);
+            }
+        });*/
 
         review.setText(Post.get(position).gettext());
         nickName.setText(Post.get(position).getNickname());
